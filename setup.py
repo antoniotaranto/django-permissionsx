9679dea249e0e4985efc70db889db068f30954db
Robert Pogorzelski <thinkingpotato@gmail.com>
2013-12-27 22:58:22 +0000

    Drop py2.6 support.

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
