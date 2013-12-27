bb07de6e9125386382a60d90c0808a53fd71abed
Robert Pogorzelski <thinkingpotato@gmail.com>
2013-12-27 22:59:35 +0000

    Feliz Ano Novo!

--- runtests.py
+++ runtests.py
@@ -2,7 +2,7 @@
 """
 PermissionsX - Authorization for Django.
 
-:copyright: Copyright (c) 2013 by Robert Pogorzelski.
+:copyright: Copyright (c) 2013-2014 by Robert Pogorzelski.
 :license:   BSD, see LICENSE for more details.
 
 """
--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 """
 PermissionsX - Authorization for Django.
 
-:copyright: Copyright (c) 2013 by Robert Pogorzelski.
+:copyright: Copyright (c) 2013-2014 by Robert Pogorzelski.
 :license:   BSD, see LICENSE for more details.
 
 """
-
 from setuptools import setup
 from setuptools import find_packages
 
